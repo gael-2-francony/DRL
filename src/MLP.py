RL: add explore_exploit and fix gradient

src/MLP.py
@@ -5,6 +5,8 @@ def preprocessing(X):
     return X
 
 def one_hot(move):
+    if type(move) is type(2):
+        return np.eye(8)[move] #TODO should be MLP.input_shape but hardcoding until better solution
     return np.eye(move.shape[0])[np.argmax(move)]
 
 def softmax_ind(X, ind):
@@ -38,10 +40,10 @@ class MLP():
     Should be fed with a frame from the game and outputs a number between 0 and 7 corresponding
     to the direction the player should move in.
     """
-    def __init__(self, input_shape, hidden_shape, output_shape=9, learning_rate=0.0001):
-        self.Wh = np.random.uniform(low=-0.05, high=0.05, size=(np.prod(input_shape), hidden_shape))
+    def __init__(self, input_shape, hidden_shape, output_shape=8, learning_rate=0.0001):
+        self.Wh = np.random.uniform(low=-0.01, high=0.01, size=(np.prod(input_shape), hidden_shape))
         self.Bh = np.zeros(hidden_shape)
-        self.Wo = np.random.uniform(low=-0.05, high=0.05, size=(hidden_shape, output_shape))
+        self.Wo = np.random.uniform(low=-0.01, high=0.01, size=(hidden_shape, output_shape))
         self.Bo = np.zeros(output_shape)
         self.learning_rate = learning_rate
         return
@@ -63,9 +65,7 @@ class MLP():
         return -np.mean(np.sum(y_true * np.log(y_pred), axis=-1))
 
     def gradients(self, X, act_h, y_pred, y_true): #Compute gradients for weights and biases using Chain rule
-        deriv_loss = deriv_cross_entropy(y_pred, y_true)
-        zo = np.dot(act_h, self.Wo) + self.Bo 
-        deriv_y_pred = deriv_softmax(zo) * deriv_loss
+        deriv_y_pred = y_pred - y_true
         grad_wo = np.outer(act_h , deriv_y_pred)
         grad_bo = deriv_y_pred
         deriv_zo = np.dot(self.Wo, deriv_y_pred)

src/RL.py
@@ -9,22 +9,43 @@ def wrong_move(y_true):
 
 class RL_Agent():
     def __init__(self, episode_size=150):
-        self.model = MLP((SCREEN_HEIGHT_g, SCREEN_WIDTH_g), 16)
+        self.model = MLP((SCREEN_HEIGHT_g, SCREEN_WIDTH_g), 300)
+        #self.load("models/model_1185.npz")
         self.activations = []
+        self.frames = []
+        self.states_alive = []
+
         self.episode_size = episode_size
+        self.episode_decisions = np.zeros((8))
+
+        self.episodes_wins = 0
+        self.episodes_nb = 0
         self.iter = 0
-        self.nb_episodes = 0
+
+    def explore_exploit(self, prediction):
+        choice = np.random.rand()
+        if choice <= .8:
+            return prediction
+        return one_hot(round(np.random.rand() * 7))
 
     def update(self, frame, is_dead):
         frame = preprocessing(frame)
         act_h, y_pred = self.model.forward_keep_activations(frame)
+        y_pred = self.explore_exploit(y_pred)
         self.activations.append((act_h, y_pred))
+        self.frames.append(frame)
+
+        #print(y_pred)
+
+        self.episode_decisions += one_hot(y_pred)
 
         self.iter += 1
         if is_dead or self.iter == self.episode_size:
-            for activation in self.activations:
+            for activation, frame in zip(self.activations, self.frames):
                 if is_dead: # Agent is dead
+                    #y_true = np.array([0, 0, 0, 0.5, 0.5, 0, 0, 0])
                     y_true = wrong_move(one_hot(activation[1]))
+                    #print("\n", activation[1],'\n', y_true, '\n\n')
                     grads = self.model.gradients(frame, activation[0], activation[1], y_true)
                     self.model.backward(grads)
                 else:
@@ -32,9 +53,20 @@ class RL_Agent():
                     grads = self.model.gradients(frame, activation[0], activation[1], y_true)
                     self.model.backward(grads)
 
+            print(f"Episode #{self.episodes_nb} : {'LOSE' if is_dead else 'WIN'}   Decisions : {self.episode_decisions}")
+
             self.reset()
-            self.nb_episodes += 1
-            print(f"Episode #{self.nb_episodes} : {'LOSE' if is_dead else 'WIN'}")
+            self.episodes_nb += 1
+            self.episode_decisions.fill(0)
+
+            if not is_dead:
+                self.episodes_wins += 1
+
+            if self.episodes_nb % 100 == 0:
+                print(f"Last 100 episodes : {self.episodes_wins}%")
+                self.episodes_wins = 0
+
+
         return np.argmax(y_pred)
 
     def reset(self):
@@ -49,11 +81,11 @@ class RL_Agent():
         self.model.Bo = loaded['Bo']
     
     def save(self):
-        np.savez_compressed("models/model_" + str(self.nb_episodes),
-            'Wh'=self.model.Wh,
-            'Bh'=self.model.Bh,
-            'Wo'=self.model.Wo,
-            'Bo'=self.model.Bo)
+        np.savez_compressed("models/model_" + str(self.episodes_nb),
+            Wh=self.model.Wh,
+            Bh=self.model.Bh,
+            Wo=self.model.Wo,
+            Bo=self.model.Bo)
 
 
 
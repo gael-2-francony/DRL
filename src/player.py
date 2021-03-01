0cf1ebe337f4098ca1db179f11177a3cecc1c52d
Gael de Francony <defrancony@hotmail.com>
2021-03-01 17:04:17 +0100

    player: control player speed and kill it when touching borders

--- src/player.py
+++ src/player.py
@@ -19,24 +19,27 @@ class Player(pygame.sprite.Sprite):
         self.surf = pygame.Surface((20, 5))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
+        self.rect.move_ip( SCREEN_WIDTH_g // 4, SCREEN_HEIGHT_g // 2)
+        self.speed = 1
 
     def update(self, frame=None):
         if self.agent is not None:
-            self.update_agent(frame)
+            return self.update_agent(frame)
         else:
             self.update_player()
+        return True
     
     # Move the sprite based on user keypresses
     def update_player(self):
         pressed_keys = pygame.key.get_pressed()
         if pressed_keys[K_UP]:
-            self.rect.move_ip(0, -5)
+            self.rect.move_ip(0, -self.speed)
         if pressed_keys[K_DOWN]:
-            self.rect.move_ip(0, 5)
+            self.rect.move_ip(0, self.speed)
         if pressed_keys[K_LEFT]:
-            self.rect.move_ip(-5, 0)
+            self.rect.move_ip(-self.speed, 0)
         if pressed_keys[K_RIGHT]:
-            self.rect.move_ip(5, 0)
+            self.rect.move_ip(self.speed, 0)
 
         # Keep player on the screen
         if self.rect.left < 0:
@@ -54,30 +57,35 @@ class Player(pygame.sprite.Sprite):
             self.agent.update(frame, True)
     
     def update_agent(self, frame):
-        move = self.agent.update(frame, False)
+        move = self.agent.update(frame, False) + 1
         if move == 1: #UP
-            self.rect.move_ip( 0, -5)
+            self.rect.move_ip( 0, -self.speed)
         if move == 2: # DOWN
-            self.rect.move_ip( 0,  5)
+            self.rect.move_ip( 0,  self.speed)
         if move == 3: # LEFT
-            self.rect.move_ip(-5,  0)
+            self.rect.move_ip(-self.speed,  0)
         if move == 4: # RIGHT
-            self.rect.move_ip( 5,  0)
+            self.rect.move_ip( self.speed,  0)
         if move == 5: # UP/Left
-            self.rect.move_ip(-5, -5)
+            self.rect.move_ip(-self.speed, -self.speed)
         if move == 6: # UP/RIGHT
-            self.rect.move_ip( 5, -5)
+            self.rect.move_ip( self.speed, -self.speed)
         if move == 7: # DOWN/LEFT
-            self.rect.move_ip(-5,  5)
+            self.rect.move_ip(-self.speed,  self.speed)
         if move == 8: # DOWN/RIGHT
-            self.rect.move_ip( 5,  5)
+            self.rect.move_ip( self.speed,  self.speed)
 
         # Keep player on the screen
         if self.rect.left < 0:
             self.rect.left = 0
+            return False
         if self.rect.right > SCREEN_WIDTH_g:
             self.rect.right = SCREEN_WIDTH_g
+            return False
         if self.rect.top <= 0:
             self.rect.top = 0
+            return False
         if self.rect.bottom >= SCREEN_HEIGHT_g:
             self.rect.bottom = SCREEN_HEIGHT_g
+            return False
+        return True

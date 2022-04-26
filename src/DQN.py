06ce92dafe8e720300adfe4c25efb40e0d55f42c
Celian GOSSEC <celian.gossec@gmail.com>
2022-04-26 16:11:33 +0200

    Train DQN on batches to take advantage of vectorization

--- src/DQN.py
+++ src/DQN.py
@@ -12,8 +12,9 @@ REPLAY_SIZE = 100
 BATCH_SIZE = 30
 
 class DQN():
-    def __init__(self, input_shape=(SCREEN_HEIGHT_g, SCREEN_WIDTH_g, 1)):
+    def __init__(self, input_shape=(SCREEN_WIDTH_g, SCREEN_HEIGHT_g, 1), output_shape=2):
         self.input_shape = input_shape
+        self.output_shape = output_shape
         self.model = self.get_model()
         self.replay_memory = deque(maxlen=REPLAY_SIZE)
 
@@ -23,8 +24,8 @@ class DQN():
         self.epsilon_min = 0.05
         self.epsilon_decay = 0.9995
 
-        self.prev_state = None
-        self.prev_action = None
+        self.last_state = None
+        self.last_action = None
 
     def get_model(self):
        state = Input(shape=self.input_shape)
@@ -33,14 +34,14 @@ class DQN():
        x = Conv2D(32, 3, activation="relu")(x)
        x = Flatten()(x)
        x = Dense(32, activation="relu")(x)
-       action = Dense(9, activation="linear")(x)
+       action = Dense(self.output_shape, activation="linear")(x)
        model = Model(inputs=state, outputs=action)
        model.compile(optimizer=Adam(lr=0.001), loss="mse")
        return model
 
     def act(self, state):
         if np.random.random_sample() < self.epsilon:
-            action = np.random.randint(9)
+            action = np.random.randint(self.output_shape)
         else:
             action = self.model.predict(np.array([state]))
             action = np.argmax(action[0])
@@ -48,30 +49,32 @@ class DQN():
         self.last_state = state
         return action
 
-    def memorize(self, state, action, reward, new_state, terminal_state):
+    def memorize(self, state, action, reward, new_state, is_terminal_state):
         try:
-            if state.all() != None and action != None:
-                self.replay_memory.append((state, action, reward, new_state, terminal_state))
+            if state is not None and new_state is not None and action is not None:
+                self.replay_memory.append((state, action, reward, new_state, is_terminal_state))
         except:
             pass
 
     def replay(self):
         batch = random.sample(self.replay_memory, BATCH_SIZE)
-        for state, action, reward, new_state, terminal_state in batch:
-            if terminal_state:
+        states = []
+        targets = []
+        for state, action, reward, new_state, is_terminal_state in batch:
+            if is_terminal_state:
                 target = reward
             else:
-                target = reward + self.discount_factor * \
-                    np.amax(self.model.predict(np.array([new_state]))[0])
+                target = reward + self.discount_factor * np.amax(self.model.predict(np.array([new_state])))
             updated_action_reward = self.model.predict(np.array([state]))
-            # Change value of action, we now have better target
-            updated_action_reward[0][action] = target
-            self.model.fit(np.array([state]), updated_action_reward, verbose=0)
+            updated_action_reward[0][action] = min(target, 1000)
+            targets.append(updated_action_reward)
+            states.append(state)
+        self.model.fit(np.array(states), np.array(targets), verbose=0)
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
 
     def can_replay(self):
-        return len(self.replay_memory) > BATCH_SIZE
+        return len(self.replay_memory) == REPLAY_SIZE
 
     def save(self, name="dqn"):
         self.model.save_weights(f"{name}.h5")
